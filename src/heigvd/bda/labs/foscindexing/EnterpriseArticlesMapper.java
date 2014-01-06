ae51726bc2753f5c510cda99a56d209fb394e0c8
komanda.phanzu <komanda.phanzu@epfl.ch>
2014-01-06 21:29:45 +0100

    (Enterprise|Person)Articles

---
+++ src/heigvd/bda/labs/foscindexing/EnterpriseArticlesMapper.java
@@ -0,0 +1,58 @@
+package heigvd.bda.labs.foscindexing;
+
+import heigvd.bda.labs.utils.EnterpriseArticles;
+import heigvd.bda.labs.utils.Name;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class EnterpriseArticlesMapper extends Mapper<LongWritable, Text, Name, EnterpriseArticles> {
+	EnterpriseArticles nameArticles;
+	Name person;
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		Configuration conf = context.getConfiguration();
+		
+		
+		person = new Name();
+		nameArticles = new EnterpriseArticles();
+		
+		super.setup(context);
+	}
+
+	//input "Enterprise\tPerson1 Names 1:1, 2, 3, 4, 5, 6;Person2 Names 2:1, 2, 3, 4, 5, 6;"
+	//output "Person1 Names1\tEnterprise1:1, 2, 3, 4, 5, 6;Enterprise1:1, 2, 3, 4, 5, 6;"
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+		throws java.io.IOException, InterruptedException {
+		
+		String[] enterpiseInfo= value.toString().split("\t");
+		Text enterprise=new Text(enterpiseInfo[0]);
+		String[] entries = enterpiseInfo[0].split(";");
+		
+		for(String e:entries){
+
+			String[] personInfo=e.split(":");
+			String[] names=personInfo[0].split(" ");
+			String[] articleIds=personInfo[1].split(",");
+
+			for(String n:names)
+				person.addName(n.trim());
+			for(String articleId:articleIds)
+				nameArticles.add(enterprise, Long.parseLong(articleId));
+			
+			
+			context.write(person, nameArticles);
+			
+			person.clear();
+			nameArticles.clear();
+			
+		}
+	}
+}
